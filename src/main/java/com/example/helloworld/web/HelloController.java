4ccfe45407c15881a81efbefe3530e92303d42f2
unknown <lanawwk@gmail.com>
2020-10-12 12:11:10 +0300

    Heroku test app

---
+++ src/main/java/com/example/helloworld/web/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.helloworld.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class HelloController {
+	@RequestMapping("/hello")
+	public String hello(@RequestParam(name="firstName") String firstName) {
+		return "Hello " + firstName;
+	}
+}
